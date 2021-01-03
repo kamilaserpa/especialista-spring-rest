276d3b0beb6c28d6c0f269fd59227f2380d464a1
Kamila Serpa <kamila.serpa@dellead.com>
2021-01-03 15:26:25 -0300

    2.25. Criando e acessando propriedades customizadas com @Value

--- src/main/java/com/kamila/food/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/kamila/food/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.kamila.food.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +11,17 @@ import com.kamila.food.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	public NotificadorEmail() {
-		System.out.println("NotificadorEmail REAL!");
-	}
+	@Value("${notificador.email.host-servidor}")
+	private String host;
+	
+	@Value("${notificador.email.porta-servidor}")
+	private String porta;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Host: " + host);
+		System.out.println("Porta: " + porta);
+		
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
