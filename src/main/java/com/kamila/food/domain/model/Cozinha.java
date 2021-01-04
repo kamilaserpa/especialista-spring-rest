0112ce3d1be9d7de3fea037c27f8281a4fb6f16c
Kamila Serpa <kamila.serpa@dellead.com>
2021-01-03 22:12:28 -0300

    3.6. Mapeando o id da entidade para autoincremento

--- src/main/java/com/kamila/food/domain/model/Cozinha.java
+++ src/main/java/com/kamila/food/domain/model/Cozinha.java
@@ -2,6 +2,8 @@ package com.kamila.food.domain.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,7 +12,7 @@ import javax.persistence.Table;
 public class Cozinha {
 
 	@Id
-	@Column(name = "id_cozinha")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idCozinha;
 
 	@Column(name = "nm_cozinha", length = 30)
--- src/main/java/com/kamila/food/domain/model/Restaurante.java
+++ src/main/java/com/kamila/food/domain/model/Restaurante.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,6 +14,7 @@ import javax.persistence.Table;
 public class Restaurante {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idRestaurante;
 
 	@Column(name = "nm_restaurante", length = 50)
