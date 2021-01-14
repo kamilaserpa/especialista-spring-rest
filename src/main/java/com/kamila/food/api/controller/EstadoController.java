d4f24b92e5e124f28df415064578735d13a24739
Kamila Serpa <kamila.serpa@dellead.com>
2021-01-13 21:32:04 -0300

    4.11. Desafio: collection resource de estados

---
+++ src/main/java/com/kamila/food/api/controller/EstadoController.java
@@ -0,0 +1,25 @@
+package com.kamila.food.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kamila.food.domain.model.Estado;
+import com.kamila.food.domain.repository.EstadoRepository;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@GetMapping
+	public List<Estado> listar() {
+		return estadoRepository.listar();
+	}
+	
+}
