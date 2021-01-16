83e7a5cd74b1e584a123c8d24cd3cc1e8e6216ee
Kamila Serpa <kamila.serpa@dellead.com>
2021-01-16 14:39:46 -0300

    4.19. Definindo o status da resposta HTTP com @ResponseStatus

--- src/main/java/com/kamila/food/api/controller/CozinhaController.java
+++ src/main/java/com/kamila/food/api/controller/CozinhaController.java
@@ -3,10 +3,12 @@ package com.kamila.food.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kamila.food.api.model.CozinhasXmlWrapper;
@@ -30,6 +32,7 @@ public class CozinhaController {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 	
+	@ResponseStatus(HttpStatus.OK)
 	@GetMapping("/{idCozinha}")
 	public Cozinha buscar(@PathVariable Long idCozinha) {
 		return cozinhaRepository.buscar(idCozinha);
