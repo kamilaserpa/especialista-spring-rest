a43934c09518994d69267913460437786eb4ab41
Kamila Serpa <kamila.serpa@dellead.com>
2021-01-16 15:20:26 -0300

    4.23. Modelando e implementando a inclusão de recursos com POST

--- src/main/java/com/kamila/food/api/controller/CozinhaController.java
+++ src/main/java/com/kamila/food/api/controller/CozinhaController.java
@@ -3,11 +3,15 @@ package com.kamila.food.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kamila.food.api.model.CozinhasXmlWrapper;
@@ -38,6 +42,12 @@ public class CozinhaController {
 			return ResponseEntity.ok(cozinha);
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha salvar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
 	}
 	
 }
