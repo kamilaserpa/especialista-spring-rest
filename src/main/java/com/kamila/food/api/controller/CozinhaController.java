93d10afd0c2c1bc35e7fb4eeb4faf50c632d4a28
Kamila Serpa <kamila.serpa@dellead.com>
2021-01-16 17:22:21 -0300

    4.26. Modelando e implementando a exclusão de recursos com DELETE
     - DataIntegrityViolationException, exceção para objeto que não pode ser deletado por ter entidades relacionadas

--- src/main/java/com/kamila/food/api/controller/CozinhaController.java
+++ src/main/java/com/kamila/food/api/controller/CozinhaController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,6 +69,21 @@ public class CozinhaController {
 			return ResponseEntity.ok(cozinhaAtual);
 		}
 		return ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{idCozinha}")
+	public ResponseEntity<Cozinha> remover(@PathVariable Long idCozinha) {
+		try {
+			Cozinha cozinha = cozinhaRepository.buscar(idCozinha);
+			if (cozinha != null) {
+				cozinhaRepository.remover(cozinha);
+				return ResponseEntity.noContent().build();
+			}
+			return ResponseEntity.notFound().build();
+		} catch (DataIntegrityViolationException e) {
+			// Caso entidade não possa ser deletada por estar relacionada com outra(s) (foreign key)
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
 	}
 
 }
