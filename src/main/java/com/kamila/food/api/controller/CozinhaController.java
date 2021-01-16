1ad8205107e81bbb5a9ae23d9acb38a7b4daa189
Kamila Serpa <kamila.serpa@dellead.com>
2021-01-16 14:51:08 -0300

    4.20. Manipulando a resposta HTTP com ResponseEntity

--- src/main/java/com/kamila/food/api/controller/CozinhaController.java
+++ src/main/java/com/kamila/food/api/controller/CozinhaController.java
@@ -3,12 +3,13 @@ package com.kamila.food.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kamila.food.api.model.CozinhasXmlWrapper;
@@ -32,10 +33,19 @@ public class CozinhaController {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 	
-	@ResponseStatus(HttpStatus.OK)
 	@GetMapping("/{idCozinha}")
-	public Cozinha buscar(@PathVariable Long idCozinha) {
-		return cozinhaRepository.buscar(idCozinha);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long idCozinha) {
+		Cozinha cozinha = cozinhaRepository.buscar(idCozinha);
+		
+//		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+//		return ResponseEntity.ok(cozinha);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+		
+		return ResponseEntity.status(HttpStatus.FOUND)
+				.headers(headers)
+				.build();
 	}
 	
 }
