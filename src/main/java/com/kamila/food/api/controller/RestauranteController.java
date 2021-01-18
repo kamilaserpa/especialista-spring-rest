961e3588cd74fa1752b9784774e5b1a75992e7c2
Kamila Serpa <kamila.serpa@dellead.com>
2021-01-18 19:23:53 -0300

    4.31. Desafio: Modelando e implementando a atualização de recursos de restaurantes

--- src/main/java/com/kamila/food/api/controller/RestauranteController.java
+++ src/main/java/com/kamila/food/api/controller/RestauranteController.java
@@ -2,12 +2,14 @@ package com.kamila.food.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -52,6 +54,27 @@ public class RestauranteController {
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 			
+		}
+	}
+	
+	@PutMapping("/{idRestaurante}")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<?> atualizar(@PathVariable Long idRestaurante,
+			@RequestBody Restaurante restaurante) {
+		try {
+
+			Restaurante restauranteAtual = restauranteRepository.buscar(idRestaurante);
+			
+			if (restauranteAtual != null) {
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+				cadastroRestauranteService.salvar(restauranteAtual);
+
+				return ResponseEntity.ok(restauranteAtual);
+			}
+			
+			return ResponseEntity.notFound().build();
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
 
