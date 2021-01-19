a404697e3ade5712ac6914f7bff93ec048610878
Kamila Serpa <kamila.serpa@dellead.com>
2021-01-18 22:09:09 -0300

    4.34. Finalizando a atualização parcial com a API de Reflections do Spring

--- src/main/java/com/kamila/food/api/controller/RestauranteController.java
+++ src/main/java/com/kamila/food/api/controller/RestauranteController.java
@@ -1,5 +1,6 @@
 package com.kamila.food.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kamila.food.domain.exception.EntidadeNaoEncontradaException;
 import com.kamila.food.domain.model.Restaurante;
 import com.kamila.food.domain.repository.RestauranteRepository;
@@ -90,13 +93,29 @@ public class RestauranteController {
 			if (restauranteAtual == null) {
 				return ResponseEntity.notFound().build();
 			}
-			campos.forEach((nomePropriedade, valorPropriedade) -> {
-
-			});
-			return ResponseEntity.ok().build();
+			merge(campos, restauranteAtual);
+			
+			return atualizar(idRestaurante, restauranteAtual);
+			
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
+	}
+
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+		// Utilizando  ObjectMapper para criar objeto tipado Restaurante
+		ObjectMapper objectMapper = new ObjectMapper();
+		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+		
+		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+			// findField: Retorna instancia de um campo
+			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+			field.setAccessible(true);
+			// GetField: Captura o valor do campo
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+			
+			ReflectionUtils.setField(field, restauranteDestino, novoValor);
+		});
 	}
 
 }
