41a4a06aacdd1aef65cf2b671077925f5b67641f
Kamila Serpa <kamila.serpa@dellead.com>
2021-01-20 23:17:14 -0300

    5.14. Adicionando restrições na cláusula where com Criteria API

--- src/main/java/com/kamila/food/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/kamila/food/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -8,6 +8,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -26,7 +28,13 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		criteria.from(Restaurante.class);
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+		
+		Predicate nomePredicate = builder.like(root.get("nmRestaurante"), "%" + nmRestaurante + "%");
+		Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+		Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+				
+		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 		
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
