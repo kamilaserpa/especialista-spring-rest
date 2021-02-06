dc06102afc22098a6cb9900823583cc18a135d7d
Kamila Serpa <kamila.serpa@dellead.com>
2021-02-06 14:17:41 -0300

    8.12. Tratando exceções em nível de controlador com @ExceptionHandler

--- src/main/java/com/kamila/food/api/controller/CidadeController.java
+++ src/main/java/com/kamila/food/api/controller/CidadeController.java
@@ -1,11 +1,14 @@
 package com.kamila.food.api.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kamila.food.api.exceptionhandler.Problema;
+import com.kamila.food.domain.exception.EntidadeNaoEncontradaException;
 import com.kamila.food.domain.exception.EstadoNaoEncontradoException;
 import com.kamila.food.domain.exception.NegocioException;
 import com.kamila.food.domain.model.Cidade;
@@ -62,13 +67,49 @@ public class CidadeController {
 		try {
 			return cadastroCidadeService.salvar(cidadeAtual);
 		} catch (EstadoNaoEncontradoException e) {
-			throw new NegocioException(e.getMessage(), e); // Alterando código de erro para 400
+			// Caso não existisse tratarNegocioException(), ao inserir a causa oriunda de EstadoNaoEncontradoException
+			// essa exceção seria capturada pelo tratarEntidadeNaoEncontradaException().
+			// Se a causa não for inserida no constructor não entrará no método tratarEntidadeNaoEncontradaException()
+			throw new NegocioException(e.getMessage());
 		}
 	}
 
 	@DeleteMapping("/{idCidade}")
 	public void remover(@PathVariable Long idCidade) {
 		cadastroCidadeService.remover(idCidade);
+	}
+	
+	/**
+	 *  Customiza corpo da resposta. Caso haja alguma exceção dentro de @ExceptionHandler está será interceptada por este método,
+	 *  no caso a resposta será um ResponseEntity.
+	 * @param e
+	 * @return
+	 */
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
+		
+		Problema problema = Problema.builder()
+				.datahora(LocalDateTime.now())
+				.mensagem(e.getMessage())
+				.build();
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+	}
+	
+	/**
+	 * Alterando representação do erro para classe Problema
+	 * @param e
+	 * @return
+	 */
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> tratarNegocioException(NegocioException e) {
+		
+		Problema problema = Problema.builder()
+				.datahora(LocalDateTime.now())
+				.mensagem(e.getMessage())
+				.build();
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
 	}
 	
 }
