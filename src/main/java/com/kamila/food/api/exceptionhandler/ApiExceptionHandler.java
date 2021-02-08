ad857ff15c9492ecea2243ecbce5c7f06430da0a
Kamila Serpa <kamila.serpa@dellead.com>
2021-02-07 23:02:04 -0300

    8.16. Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler

--- src/main/java/com/kamila/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/kamila/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -2,11 +2,13 @@ package com.kamila.food.api.exceptionhandler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.kamila.food.domain.exception.EntidadeEmUsoException;
@@ -17,20 +19,15 @@ import com.kamila.food.domain.exception.NegocioException;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	/**
-	 *  Customiza corpo da resposta. Caso haja alguma exceção dentro de @ExceptionHandler está será interceptada por este método,
-	 *  no caso a resposta será um ResponseEntity. HttpStatus 404.
+	 *  Customiza corpo da resposta. Caso haja alguma exceção dentro de @ExceptionHandler está será interceptada por este método.
+	 *  Aresposta será um ResponseEntity. HttpStatus 404.
 	 * @param e
 	 * @return
 	 */
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
-		
-		Problema problema = Problema.builder()
-				.datahora(LocalDateTime.now())
-				.mensagem(e.getMessage())
-				.build();
-		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request) {
+
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 	
 	/**
@@ -39,25 +36,39 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	 * @return
 	 */
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(NegocioException e) {
+	public ResponseEntity<?> tratarNegocioException(NegocioException ex,  WebRequest request) {
 		
-		Problema problema = Problema.builder()
-				.datahora(LocalDateTime.now())
-				.mensagem(e.getMessage())
-				.build();
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
 		
-		Problema problema = Problema.builder()
-				.datahora(LocalDateTime.now())
-				.mensagem(e.getMessage())
-				.build();
-		
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
 	}
-	
+
+	/**
+	 * Se recebe uma string como body, instancia um Problema a insere o body na mensagem.
+	 * Caso contrário, insere um problema no body com a ReasonPhrase na mensagem
+	 */
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+
+		if (body == null) {
+			body = Problema.builder()
+					.datahora(LocalDateTime.now())
+					.mensagem(status.getReasonPhrase())
+					.build();
+			
+		} else if (body instanceof String) {
+			body = Problema.builder()
+					.datahora(LocalDateTime.now())
+					.mensagem((String) body)
+					.build();
+		}
+
+		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
+
 }
