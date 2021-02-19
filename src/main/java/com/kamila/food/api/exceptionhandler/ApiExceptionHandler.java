74cb2db81855916219227fb812e4e323768ff054
Kamila Serpa <kamila.serpa@dellead.com>
2021-02-19 00:50:06 -0300

    8.26. Desafio: tratando a exceção NoHandlerFoundException

--- src/main/java/com/kamila/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/kamila/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.fasterxml.jackson.databind.JsonMappingException.Reference;
@@ -108,6 +109,21 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
 	
+	@Override
+	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		ProblemType problemType = ProblemType.RECURSO_NÃO_ENCONTRADO;
+
+		String detail = String.format("O recurso '%s', que você tentou acessar, é inexistente.",
+						ex.getRequestURL());
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+		return handleExceptionInternal(ex, problem, headers, status, request);
+	}
+	
+	
 	// CUSTOMIZANDO EXCEÇÕES CRIADAS NO PROJETO
 	
 	/**
@@ -120,7 +136,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 			WebRequest request) {
 
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
+		ProblemType problemType = ProblemType.RECURSO_NÃO_ENCONTRADO;
 		String detail = ex.getMessage();
 
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
--- src/main/java/com/kamila/food/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/kamila/food/api/exceptionhandler/ProblemType.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
-	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
+	RECURSO_NÃO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
 	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
