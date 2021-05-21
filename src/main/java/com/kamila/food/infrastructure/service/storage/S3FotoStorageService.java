c26e70c7b324224828bb8c5c8cc7d3500d644c8b
Kamila <kamilardg@gmail.com>
2021-05-21 17:54:55 -0300

    14.24. Desafio: Implementando a exclusão de objetos do bucket da Amazon S3

--- src/main/java/com/kamila/food/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/kamila/food/infrastructure/service/storage/S3FotoStorageService.java
@@ -2,6 +2,7 @@ package com.kamila.food.infrastructure.service.storage;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.kamila.food.core.storage.StorageProperties;
@@ -54,6 +55,16 @@ public class S3FotoStorageService implements FotoStorageService {
 
     @Override
     public void remover(String nomeArquivo) {
+        try {
+            String caminhoArquivo = getCaminhoArquivo(nomeArquivo);
 
+            DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(
+                    storageProperties.getS3().getBucket(),
+                    caminhoArquivo);
+
+            amazonS3.deleteObject(deleteObjectRequest);
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível excluir arquivo na AmazonS3", e);
+        }
     }
 }
