060ab64c1191531317af0bb21eadb8608257ba55
Kamila <kamilardg@gmail.com>
2021-06-30 14:09:28 -0300

    18.15. Referenciando modelo de representação de problema com códigos de
    status de erro

--- food-api/src/main/java/com/kamila/food/core/openapi/SpringFoxConfig.java
+++ food-api/src/main/java/com/kamila/food/core/openapi/SpringFoxConfig.java
@@ -14,6 +14,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.ResponseMessage;
@@ -37,9 +38,9 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.kamila.food.api"))
-                .paths(PathSelectors.any())
-                .build()
+                    .apis(RequestHandlerSelectors.basePackage("com.kamila.food.api"))
+                    .paths(PathSelectors.any())
+                    .build()
                 .useDefaultResponseMessages(false) // Desabilitando httpStatus codes error padrão
                 .globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
                 .globalResponseMessage(RequestMethod.POST, globalPostResponseMessages())
@@ -60,6 +61,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 new ResponseMessageBuilder()
                         .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
                         .message("Erro interno no servidor")
+                        .responseModel(new ModelRef("Problema")) // Referenciando o modelo de resposta em caso 400 de GetMethod
                         .build(),
                 new ResponseMessageBuilder()
                         .code(HttpStatus.NOT_ACCEPTABLE.value())
@@ -73,10 +75,12 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 new ResponseMessageBuilder()
                         .code(HttpStatus.BAD_REQUEST.value())
                         .message("Requisição inválida (erro do cliente)")
+                        .responseModel(new ModelRef("Problema"))
                         .build(),
                 new ResponseMessageBuilder()
                         .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
                         .message("Erro interno no servidor")
+                        .responseModel(new ModelRef("Problema"))
                         .build(),
                 new ResponseMessageBuilder()
                         .code(HttpStatus.NOT_ACCEPTABLE.value())
@@ -85,6 +89,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 new ResponseMessageBuilder()
                         .code(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
                         .message("Requisição recusada porque o corpo está em um formato não suportado")
+                        .responseModel(new ModelRef("Problema"))
                         .build()
         );
     }
@@ -94,10 +99,12 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 new ResponseMessageBuilder()
                         .code(HttpStatus.BAD_REQUEST.value())
                         .message("Requisição inválida (erro do cliente)")
+                        .responseModel(new ModelRef("Problema"))
                         .build(),
                 new ResponseMessageBuilder()
                         .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
                         .message("Erro interno no servidor")
+                        .responseModel(new ModelRef("Problema"))
                         .build(),
                 new ResponseMessageBuilder()
                         .code(HttpStatus.NOT_ACCEPTABLE.value())
@@ -106,6 +113,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 new ResponseMessageBuilder()
                         .code(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
                         .message("Requisição recusada porque o corpo está em um formato não suportado")
+                        .responseModel(new ModelRef("Problema"))
                         .build()
         );
     }
@@ -115,10 +123,12 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 new ResponseMessageBuilder()
                         .code(HttpStatus.BAD_REQUEST.value())
                         .message("Requisição inválida (erro do cliente)")
+                        .responseModel(new ModelRef("Problema"))
                         .build(),
                 new ResponseMessageBuilder()
                         .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
                         .message("Erro interno no servidor")
+                        .responseModel(new ModelRef("Problema"))
                         .build()
         );
     }
