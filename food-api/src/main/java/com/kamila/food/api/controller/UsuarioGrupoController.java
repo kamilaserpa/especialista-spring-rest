bc7a1b5049b3991540e979960e07837dbdb8ff14
Kamila <kamilardg@gmail.com>
2021-07-02 14:44:05 -0300

    18.39. Desafio: descrevendo documentação de endpoints de associação de
    grupos com usuários

--- food-api/src/main/java/com/kamila/food/api/controller/UsuarioGrupoController.java
+++ food-api/src/main/java/com/kamila/food/api/controller/UsuarioGrupoController.java
@@ -2,37 +2,43 @@ package com.kamila.food.api.controller;
 
 import com.kamila.food.api.assembler.GrupoModelAssembler;
 import com.kamila.food.api.model.GrupoModel;
+import com.kamila.food.api.openapi.controller.UsuarioGrupoControllerOpenApi;
 import com.kamila.food.domain.model.Usuario;
 import com.kamila.food.domain.service.CadastroUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/usuarios/{idUsuario}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(value = "/usuarios/{idUsuario}/grupos",
+		produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 
 	@Autowired
 	private CadastroUsuarioService cadastroUsuarioService;
 	
 	@Autowired
 	private GrupoModelAssembler grupoModelAssembler;
-	
+
+	@Override
 	@GetMapping
 	public List<GrupoModel> listar(@PathVariable Long idUsuario) {
 		Usuario usuario = cadastroUsuarioService.buscarOuFalhar(idUsuario);
 		
 		return grupoModelAssembler.toCollectionModel(usuario.getGrupos());
 	}
-	
+
+	@Override
 	@DeleteMapping("/{idGrupo}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void desassociarGrupo(@PathVariable Long idUsuario, @PathVariable Long idGrupo) {
 		cadastroUsuarioService.desassociarGrupo(idUsuario, idGrupo);
 	}
-	
+
+	@Override
 	@PutMapping("/{idGrupo}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void associarGrupo(@PathVariable Long idUsuario, @PathVariable Long idGrupo) {
---
+++ food-api/src/main/java/com/kamila/food/api/openapi/controller/UsuarioGrupoControllerOpenApi.java
@@ -0,0 +1,47 @@
+package com.kamila.food.api.openapi.controller;
+
+import com.kamila.food.api.exceptionhandler.Problem;
+import com.kamila.food.api.model.GrupoModel;
+import com.kamila.food.api.model.UsuarioModel;
+import io.swagger.annotations.*;
+
+import java.util.List;
+
+@Api(tags = "Usuários")
+public interface UsuarioGrupoControllerOpenApi {
+
+    @ApiOperation("Lista os grupos associados a usuário")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Usuário não encontrado", response = Problem.class)
+    })
+    List<GrupoModel> listar(
+            @ApiParam(value = "ID de um usuário", example = "1", required = true)
+                    Long idUsuario);
+
+
+    @ApiOperation("Desassociação de grupo com usuário")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Desassociação realizada com sucesso"),
+            @ApiResponse(code = 404, message = "Usuário ou grupo não encontrado", response = Problem.class)
+    })
+    void desassociarGrupo(
+            @ApiParam(value = "ID de um usuário", example = "1", required = true)
+                    Long idUsuario,
+            @ApiParam(value = "ID de um grupo", example = "1", required = true)
+                    Long idGrupo);
+
+
+    @ApiOperation("Associação de grupo com usuário")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Associação realizada com sucesso"),
+            @ApiResponse(code = 404, message = "Usuário ou grupo não encontrado",
+                    response = Problem.class)
+    })
+    void associarGrupo(
+            @ApiParam(value = "ID do usuário", example = "1", required = true)
+                    Long usuarioId,
+
+            @ApiParam(value = "ID do grupo", example = "1", required = true)
+                    Long grupoId);
+
+}
