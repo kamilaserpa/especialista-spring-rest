ede56b49ec08f16e42eed6ac66245513029ee8d6
Kamila <kamilardg@gmail.com>
2021-07-07 16:00:40 -0300

    19.19. Desafio: adicionando template variables do filtro de pedidos

--- food-api/src/main/java/com/kamila/food/api/assembler/PedidoModelAssembler.java
+++ food-api/src/main/java/com/kamila/food/api/assembler/PedidoModelAssembler.java
@@ -34,9 +34,17 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
                 new TemplateVariable("sort", VariableType.REQUEST_PARAM)
         );
 
+        TemplateVariables filtroVariables = new TemplateVariables(
+                new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
+                new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM)
+        );
+
         String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
 
-        pedidoModel.add(new Link(UriTemplate.of(pedidosUrl, pageVariables), "pedidos"));
+        pedidoModel.add(new Link(UriTemplate.of(pedidosUrl,
+                pageVariables.concat(filtroVariables)), "pedidos"));
 
         pedidoModel.getRestaurante().add(linkTo(methodOn(RestauranteController.class)
                 .buscar(pedido.getRestaurante().getId())).withSelfRel());
