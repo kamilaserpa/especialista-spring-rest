cb094766bca53286d83484d18fde1e370947c86f
Kamila <kamilardg@gmail.com>
2021-10-08 14:18:48 -0300

    23.20. Carregando as Granted Authorities e restringindo acesso a endpoints na API

--- food-api/src/main/java/com/kamila/food/core/security/ResourceServerConfig.java
+++ food-api/src/main/java/com/kamila/food/core/security/ResourceServerConfig.java
@@ -2,17 +2,23 @@ package com.kamila.food.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +28,11 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated() // Habilita obrigatoriedade de autenticação em toda a aplicação
+                .antMatchers(HttpMethod.POST, "/v1/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
+                .antMatchers(HttpMethod.PUT, "/v1/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
+                .antMatchers(HttpMethod.GET, "/v1/cozinhas/**").authenticated()
+                .anyRequest().denyAll()
+
 
                 // Spring Security permite Request PreFlight, pois ela não contém os headers de autenticação e são necessárias
                 // Liberando requisições `OPTIONS`
@@ -30,16 +40,29 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .cors()
 
                 .and()
-                .oauth2ResourceServer().jwt();
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
     }
 
-//    // Comentado para habilitar JWT Chave Assimétrica
-//    @Bean
-//    public JwtDecoder jwtDecoder() {
-//        Chave Simétrica
-//        var secretKey = new SecretKeySpec("0DEF419417A1C1B371E29B143D49D6F707D8FC008EC55E73270D1647F3CD2054".getBytes(), "HmacSHA256");
-//        return NimbusJwtDecoder.withSecretKey(secretKey).build();
-//    }
+    // Captura permissoes (authorities) de usuário contidas no Token Jwt recebido
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var authorities = jwt.getClaimAsStringList("authorities");
+
+            if (authorities == null) {
+                authorities = Collections.emptyList();
+            }
+            // Convertendo authorities recebidas pelo Token Jwt em SimpleGrantedAuthority
+            return authorities.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        });
+
+        return jwtAuthenticationConverter;
+    }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
